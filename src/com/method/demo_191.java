5b2e3e79663636578c34a360557244ea8343df15
MikeColeone <coleonemike@gmail.com>
2024-08-06 07:45:34 +0800

    update

--- src/com/method/demo_191.java
+++ src/com/method/demo_191.java
@@ -1,16 +1,29 @@
-package com.method;
-
-public class demo_191 {
-}
-class Solution {
-    public int hammingWeight(int n) {
-        int ans = 0;
-        while(n>0){
-            if(n%2==1){
-                ans++;
-            }
-            n/=2;
-        }
-        return ans;
-    }
-}
+//package com.method;
+//
+//public class demo_191 {
+//}
+//class Solution {
+//    public int hammingWeight(int n) {
+//        int ans = 0;
+//        while(n>0){
+//            if(n%2==1){
+//                ans++;
+//            }
+//            n/=2;
+//        }
+//        return ans;
+//    }
+//}
+//////位运算优化 (n)&(n-1) 的结果恰好是n的最低位1化为0的结果
+////class Solution {
+////    public int hammingWeight(int n) {
+////        int ans = 0;
+////        while(n>0){
+////            if(n%2==1){
+////                ans++;
+////            }
+////            n/=2;
+////        }
+////        return ans;
+////    }
+////}
