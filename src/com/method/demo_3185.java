a889b9a0d6104fcf8d22a74fb6d3619183e3c576
MikeColeone <coleonemike@gmail.com>
2024-11-08 19:41:11 +0800

    update

--- src/com/method/demo_3185.java
+++ src/com/method/demo_3185.java
@@ -27,14 +27,14 @@ public class demo_3185 {
 
 //通过取模操作实现减少判断0和本身 同时实现可以少申请一点空间
 
-class Solution {
-    public long countCompleteDayPairs(int[] hours) {
-        int[] cnt = new int[24]; //直接存储和当前数位相加是倍数的出现数量
-        long ans = 0;
-        for (int hour : hours) {
-            ans += cnt[(24 - hour % 24)%24];
-            cnt[hour % 24]++;
-        }
-        return ans;
-    }
-}
+//class Solution {
+//    public long countCompleteDayPairs(int[] hours) {
+//        int[] cnt = new int[24]; //直接存储和当前数位相加是倍数的出现数量
+//        long ans = 0;
+//        for (int hour : hours) {
+//            ans += cnt[(24 - hour % 24)%24];
+//            cnt[hour % 24]++;
+//        }
+//        return ans;
+//    }
+//}
