39bc04ec0e7bb1343e4d8c3d2408e7ef681bb835
MikeColeone <coleonemike@gmail.com>
2025-03-14 16:39:56 +0800

    demo_94:二叉树的中序遍历

--- src/com/method/demo_147.java
+++ src/com/method/demo_147.java
@@ -1,23 +1,29 @@
-package com.method;
-
-import com.entity.*;
-/*对链表进行插入排序*/
-public class demo_147 {
-}
-
-
-/**
- * Definition for singly-linked list.
- * public class ListNode {
- *     int val;
- *     ListNode next;
- *     ListNode() {}
- *     ListNode(int val) { this.val = val; }
- *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
- * }
- */
-class Solution {
-    public ListNode insertionSortList(ListNode head) {
-
-    }
-}
+//package com.method;
+//
+//import com.entity.*;
+///*对链表进行插入排序*/
+//public class demo_147 {
+//}
+//
+//
+///**
+// * Definition for singly-linked list.
+// * public class ListNode {
+// *     int val;
+// *     ListNode next;
+// *     ListNode() {}
+// *     ListNode(int val) { this.val = val; }
+// *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+// * }
+// */
+//class Solution {
+//    public ListNode insertionSortList(ListNode head) {
+//        if(head == null) return null;
+//        ListNode dummyHead = new ListNode(0);
+//        dummyHead.next = head;
+//        while(dummyHead.next != null) {
+//            ListNode cur = dummyHead;
+//        }
+//
+//    }
+//}
---
+++ src/com/method/demo_94.java
@@ -0,0 +1,39 @@
+package com.method;
+import com.entity.*;
+
+import java.util.ArrayList;
+import java.util.Deque;
+import java.util.LinkedList;
+import java.util.List;
+
+/*二叉树的中序遍历*/
+/*非递归算法*/
+public class demo_94 {
+}
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+//class Solution {
+//    public List<Integer> inorderTraversal(TreeNode root) {
+//        List<Integer> list = new ArrayList<Integer>();
+//        Deque<Node> stack = new LinkedList<Node>();
+//        if()
+//    }
+//}
+
+
+/*前序遍历：中，左，右
+中序遍历：左，中，右
+后序遍历：左，右，中*/
