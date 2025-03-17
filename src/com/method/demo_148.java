2ada96159abbc1e45d1222fd2c80bc48360c5bd3
MikeColeone <coleonemike@gmail.com>
2025-03-17 15:58:54 +0800

    demo_283:新方法

--- src/com/method/demo_148.java
+++ src/com/method/demo_148.java
@@ -1,14 +1,14 @@
-package com.method;
-import com.entity.*;
-public class demo_148 {
-}
-
-class Solution {
-    public ListNode sortList(ListNode head) {
-
-        ListNode dummyHead = new ListNode(0);
-        dummyHead.next = head;
-        ListNode pre = dummyHead;
-
-    }
-}
+//package com.method;
+//import com.entity.*;
+//public class demo_148 {
+//}
+//
+//class Solution {
+//    public ListNode sortList(ListNode head) {
+//
+//        ListNode dummyHead = new ListNode(0);
+//        dummyHead.next = head;
+//        ListNode pre = dummyHead;
+//
+//    }
+//}
--- src/com/method/demo_283.java
+++ src/com/method/demo_283.java
@@ -2,10 +2,11 @@
 //
 //import java.util.Arrays;
 //
+///*移动零*/
 ///*移动0双指针*/
 //public class demo_283 {
 //    public static void main(String[] args) {
-//        int[] arr = {1,2,4,7,0,0};
+//        int[] arr = {1,2,4,7,0,0,1};
 //
 //        Solution solution = new Solution();
 //        solution.moveZeroes(arr);
@@ -14,20 +15,38 @@
 //
 //}
 //
-//
+////
+////class Solution {
+////    public void moveZeroes(int[] nums) {
+////        int n = nums.length;
+////        int l = 0, r = 0;
+////        while(r<n){
+////            int t = 0;
+////            if(nums[r]!=0){
+////                t = nums[r];
+////                nums[r] = nums[l];
+////                nums[l] = t;
+////                l++;
+////            }
+////            r++;
+////        }
+////
+////    }
+////}
 //class Solution {
 //    public void moveZeroes(int[] nums) {
 //        int n = nums.length;
-//        int l = 0, r = 0;
-//        while(r<n){
-//            int t = 0;
-//            if(nums[r]!=0){
-//                t = nums[r];
-//                nums[r] = nums[l];
-//                nums[l] = t;
-//                l++;
+//        int cur = 0;
+//
+//        for(int i = 0;i<n;i++){
+//            if(nums[i]!=0){
+//                nums[cur]=nums[i];
+//                cur++;
 //            }
-//            r++;
+//        }
+//
+//        for (int i = cur; i < n; i++) {
+//            nums[i]=0;
 //        }
 //
 //    }
