6ec4d3b3fc39686bb9fe53dab3747091214f5245
MikeColeone <coleonemike@gmail.com>
2025-03-25 17:05:08 +0800

    demo:2715:定期取消

--- src/com/method/demo_148.java
+++ src/com/method/demo_148.java
@@ -1,14 +1,45 @@
-//package com.method;
-//import com.entity.*;
-//public class demo_148 {
-//}
-//
+package com.method;
+import com.entity.*;
+public class demo_148 {
+}
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
 //class Solution {
 //    public ListNode sortList(ListNode head) {
+//        if (head == null || head.next == null) {
+//            return head;
+//        }
 //
-//        ListNode dummyHead = new ListNode(0);
-//        dummyHead.next = head;
-//        ListNode pre = dummyHead;
+//        boolean swapped;
+//        ListNode current;
+//        ListNode tail = null;
 //
+//        do {
+//            swapped = false;
+//            current = head;
+//
+//            while (current.next != tail) {
+//                if (current.val > current.next.val) {
+//                    // Swap the values
+//                    int temp = current.val;
+//                    current.val = current.next.val;
+//                    current.next.val = temp;
+//                    swapped = true;
+//                }
+//                current = current.next;
+//            }
+//            tail = current;
+//        } while (swapped);
+//
+//        return head;
 //    }
 //}
