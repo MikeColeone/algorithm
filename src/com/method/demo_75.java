1bbac3fb304602580af24406bf451e32a4c7b1ad
MikeColeone <coleonemike@gmail.com>
2025-04-14 15:32:30 +0800

    demo:118杨辉三角

--- src/com/method/demo_75.java
+++ src/com/method/demo_75.java
@@ -1,19 +1,19 @@
-package com.method;
-
-import java.util.Arrays;
-
-public class demo_75 {
-}
-class Solution {
-    public void sortColors(int[] nums) {
-        nums = Arrays.stream(nums).sorted().toArray();
-    }
-}
-
-
-
-class Solution {
-    public void sortColors(int[] nums) {
-        Arrays.sort(nums);  // 直接对原数组排序
-    }
-}
+//package com.method;
+//
+//import java.util.Arrays;
+//
+//public class demo_75 {
+//}
+//class Solution {
+//    public void sortColors(int[] nums) {
+//        nums = Arrays.stream(nums).sorted().toArray();
+//    }
+//}
+//
+//
+//
+//class Solution {
+//    public void sortColors(int[] nums) {
+//        Arrays.sort(nums);  // 直接对原数组排序
+//    }
+//}
