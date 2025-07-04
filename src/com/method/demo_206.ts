f44317123d07c89845cdf7f69faea78d2c990ce0
MikeColeone <coleonemike@gmail.com>
2025-07-04 23:22:57 +0800

    feat: 递归反转链表

--- src/com/method/demo_206.ts
+++ src/com/method/demo_206.ts
@@ -1,13 +1,27 @@
+//
+// function reverseList(head: ListNode | null): ListNode | null {
+//     let pre: ListNode | null = null;
+//     let cur: ListNode | null = head;
+//
+//     while (cur != null) {
+//         const temp: ListNode | null = cur.next;
+//         cur.next = pre;
+//         pre = cur;
+//         cur = temp;
+//     }
+//     return pre;
+// };
+
+
+
+
 
 function reverseList(head: ListNode | null): ListNode | null {
-    let pre: ListNode | null = null;
-    let cur: ListNode | null = head;
-
-    while (cur != null) {
-        const temp: ListNode | null = cur.next;
-        cur.next = pre;
-        pre = cur;
-        cur = temp;
+    if (head === null || head.next === null) {
+        return head;
     }
-    return pre;
+    const newHead = reverseList(head.next);
+    head.next.next = head;
+    head.next = null;
+    return newHead;
 };
