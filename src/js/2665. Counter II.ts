2d64003d18e55ba6ee42085467c1368134820bc2
MikeColeone <coleonemike@gmail.com>
2025-07-23 22:43:21 +0800

    feat: 计数器

--- src/js/2665. Counter II.ts
+++ src/js/2665. Counter II.ts
@@ -1,1 +1,25 @@
 //计数器Ⅱ
+
+type Counter = {
+    increment: () => number,
+    decrement: () => number,
+    reset: () => number,
+}
+
+function createCounter(init: number): Counter {
+    let temp = init;
+    return {
+    increment: function (): number {
+        temp++;
+        return temp;
+    },
+    decrement: function (): number {
+        temp--;
+        return temp;
+    },
+    reset: function (): number {
+        temp = init;
+        return temp;
+    }
+}
+};
