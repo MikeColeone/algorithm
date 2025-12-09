570290442199b4a9b618aa76197b17388a188027
MikeColeone <coleonemike@gmail.com>
2025-12-09 22:37:26 +0800

    update

--- src/solutions/0001-0099/2. 两数相加.ts
+++ src/solutions/0001-0099/2. 两数相加.ts
@@ -1,26 +1,13 @@
 class ListNode {
-    val: number
-    next: ListNode | null
-    constructor(val?: number, next?: ListNode | null) {
-        this.val = (val===undefined ? 0 : val)
-        this.next = (next===undefined ? null : next)
-    }
-}
-
-function addTwoNumbers(l1: ListNode | null, l2: ListNode | null): ListNode | null {
-    return null;
-};
-
-
-
-export function reverseList(head: ListNode | null): ListNode | null {
-  let prev: ListNode | null = null;
-  let curr: ListNode | null = head;
-  while (curr) {
-    const nextNode = curr.next;
-    curr.next = prev;
-    prev = curr;
-    curr = nextNode;
+  val: number;
+  next: ListNode | null;
+  constructor(val?: number, next?: ListNode | null) {
+    this.val = val === undefined ? 0 : val;
+    this.next = next === undefined ? null : next;
   }
-  return prev;
 }
+
+function addTwoNumbers(
+  l1: ListNode | null,
+  l2: ListNode | null
+): ListNode | null {}
